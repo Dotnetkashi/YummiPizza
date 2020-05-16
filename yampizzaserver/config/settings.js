75e84ecff4b98dd47b3e0f5d6fb181e8a3bb1cc3
DotnetSahooKashi <sahookahsinath92@gmail.com>
2020-05-16 09:59:27 +0530

    add mlab databse service for mongodb to work in production

--- yampizzaserver/config/settings.js
+++ yampizzaserver/config/settings.js
@@ -5,7 +5,7 @@ let rootPath = path.normalize(path.join(__dirname, '/../'))
 module.exports = {
   development: {
     rootPath: rootPath,
-    db: 'mongodb://localhost:27017/PizzaLab',
+    db: "mongodb://admin:YWXdaakODbtBdSDs@gcmongodb-shard-00-00-ma1ze.gcp.mongodb.net:27017,gcmongodb-shard-00-01-ma1ze.gcp.mongodb.net:27017,gcmongodb-shard-00-02-ma1ze.gcp.mongodb.net:27017/test?ssl=true&replicaSet=GcMongoDb-shard-0&authSource=admin&retryWrites=true&w=majority",
     port: 5000
   },
   staging: {
